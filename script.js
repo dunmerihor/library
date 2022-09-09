e3e8532bc8dd33de17602b9d50923b55b5661995
dunmeri <ethanq95@gmail.com>
2022-09-09 13:22:13 +0700

    Rewrite with use of classes

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let myBooks = [];
 const bookForm = document.querySelector('.book-form');
 bookForm.addEventListener('click', e => e.stopPropagation());
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.info = function() {
+    info() {
     return `${this.title} by ${this.author}, ${this.pages} pages, `;
+    }
 }
 
 function showForm() {
